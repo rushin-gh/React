68c0693c55e14f31f9e44b98c215883c389dd58a
rushin-gh <rushin.mailme@gmail.com>
2025-08-21 00:40:44 +0530

    Everything about Parcel

--- app.js
+++ app.js
@@ -1,58 +1,25 @@
-// const elementOne = React.createElement("div", {id: "level1"}, "Ohh");
-// const elementTwo = React.createElement("div", {id: "level2"}, "My");
-// const elementThree = React.createElement("div", {id: "level3"}, "God!");
-
-// ReactDOM.createRoot(document.getElementById("level1")).render(element2);
-// // ReactDOM.createRoot(document.getElementById("level2")).render(element3);
-
-
-// // elementOne.createRoot(document.getElementById("level1")).render(element2);
-// // elementTwo.createRoot(document.getElementById("level1")).render(element3);
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const ele = React.createElement(
     "div",
     {id:"Grand"},
-    [
-        React.createElement(
-            "div",
-            {id:"Parent"},
-            [
-                React.createElement(
-                    "h1",
-                    {id:"Child1"},
-                    "First Child"
-                ),
-                React.createElement(
-                    "h2",
-                    {id:"Child2"},
-                    "Second Child"
-                )
-            ]
-        ),
-        React.createElement(
-            "div",
-            {id:"Parent2"},
-            [
-                React.createElement(
-                    "a",
-                    {id:"Child1", href:"https://google.com"},
-                    "Mixed Childs"
-                )
-                // ,
-                // React.createElement(
-                //     "h1",
-                //     {id:"Child1"},
-                //     "First Child"
-                // ),
-                // React.createElement(
-                //     "h2",
-                //     {id:"Child2"},
-                //     "Second Child"
-                // )
-            ]
-        )
-    ]
+    React.createElement(
+        "div",
+        {id:"Parent"},
+        [
+            React.createElement(
+                "h1",
+                {id:"Child1", key:"Child1"},
+                "First Child"
+            ),
+            React.createElement(
+                "h2",
+                {id:"Child2", key:"Child2"},
+                "Second Child"
+            )
+        ]
+    )
 )
 
 ReactDOM.createRoot(document.getElementById("root")).render(ele);
