fcfcb5e59a5317315dc3557abec8bc1496a716be
rushin-gh <rushin.mailme@gmail.com>
2025-08-23 12:25:38 +0530

    react basics

--- app.js
+++ app.js
@@ -31,7 +31,68 @@ const element = (
         This is div
     </div>
 );
-ReactDOM.createRoot(document.getElementById("root")).render(element);
 
 // setTimeout(() => {
-// }, 5000);
+    // }, 5000);
+    
+// const HeadingComponent = () => {
+//     return (
+//         <h3>
+//             This is Heading Component
+//         </h3>
+//     )
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const Paragraph = () =>
+(
+    <p>Hello bosdiwalo <br/> Bhago yahase <br/> behene ke laudo.</p>
+)
+
+const HeadingComponent = () => 
+(
+    <h3>
+        <Paragraph/>
+        This is Heading Component 2
+    </h3>
+)
+
+
+root.render(<HeadingComponent/>);
+
+
+const Head1 = () => (
+    <h1>This is H1 tag.</h1>
+)
+
+const Head2 = () => (
+    <h2>This is H2 tag.</h2>
+)
+
+const Head3 = () => (
+    <h3>This is H3 tag.</h3>
+)
+
+const reactHeading = (
+    <h1>React heading</h1>
+)
+
+const Headers = () => (
+    <>
+        <Head1/>
+    </>
+);
+
+const spanTag = (
+    <>
+    {Headers()}
+    <Headers/>
+    <Headers></Headers>
+    </>
+)
+
+
+// setTimeout(() => {
+    root.render(spanTag);
+// }, 2000);
